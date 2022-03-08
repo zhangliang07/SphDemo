b929bbc671320b3f0dcc2901762f8c8d938be206
南极小强 <mygod1988@126.com>
2022-03-08 19:26:40 +0800

    update

--- SphDemo/Partical.py
+++ SphDemo/Partical.py
@@ -1,10 +1,19 @@
 ﻿import math
+import kenelFcunction
 from typing import List
 
 
 
-__initDensity = 100
-__viscosity : float= 100
+#1. compute location of every particals
+#2. compute density and pressrue of every particals
+#3. compute pressure, Viscous coefficient of every particals
+#4. compute velocity of every particals according to the total force of every particals
+#5. continue step 1 
+
+__c = 200
+__initDensity : float = 100
+__viscosity : float = 100
+__gravityY : float = -9.8
 
 
 
@@ -13,10 +22,14 @@ class Partical(object):
   posY = 0
   velX = 0
   velY = 0
+  accX = 0
+  accY = 0
   mass = 100
   density = __initDensity
-  pressure = 0
 
+
+  pressure = 0
+  tauXX, tauXY, tauYY, tauYX = 0
 
 
   def __init__(self, x, y):
@@ -28,56 +41,99 @@ class Partical(object):
   def computeDensity(self, neigborList: list['Partical']) -> float:
     dpdt = 0
 
-    for other in neigborList:
-      velXDiff = self.velX - other.velX
-      velYDiff = self.velY - other.velY
-
-      dwdx, dwdy = self.kenelFunction2derivative(other);
-      dpdtofNeigbor = self.density * other.mass / other.density \
-       * (velXDiff * dwdx + velYDiff * dwdy)
-      dpdt += dpdtofNeigbor
+    for point in neigborList:
+      dwdx, dwdy = kenelFcunction.kenelDerivative(self, point)
+      dpdt += self.density * point.mass / point.density \
+       * ((self.velX - point.velX) * dwdx + (self.velY - point.velY) * dwdy)
 
     return dpdt
 
 
 
-  def computePressure(self, neigborList: list['Partical']) -> float:
-    #the important is to confine the direction of the pressure
-    #this use the pressure gradient equation from the papar of MPS
-    #and plus the power of mass 
-
-    gradientX = 0
-    gradientY = 0
-
-    for other in neigborList:
-      w = self.kenelFunction(other)
-      distance = math.sqrt( (self.posX - other.posX) **2 + (self.posY - other.posY) **2 )
-      temp = other.mass / other.density * w * (self.pressure - other.pressure) / distance
-      gradientX += temp * (self.posX - other.posX)
-      gradientY += temp * (self.posY - other.posY)
-
-    gradient = math.sqrt(gradientY ** 2 + gradientY **2)
-    gradientX /= gradient
-    gradientY /= gradient
-
+  def computePressrue(self):
     B = 32
-    press = B * ((self.density / __initDensity) ** 7 - 1)
-    return press * gradientX, press * gradientY
-  
+    self.pressure = B * ((self.density / __initDensity) ** 7 - 1)
+    #self.pressure = self.density * __c **2
+
+
+  def computeViscous(self, neigborList: list['Partical']):
+    dvdx = 0
+    dvdy = 0
+    for point in neigborList:
+      dwdx, dwdy = kenelFcunction.kenelDerivative(self, point)
+      dvdx += point.mass / point.density * point.velX * dwdx
+      dvdy += point.mass / point.density * point.vely * dwdy
+
+    self.tauXX = __viscosity * (2 * dvdx - 2/3 * (dvdx + dvdy))
+    self.tauYY = __viscosity * (2 * dvdy - 2/3 * (dvdx + dvdy))
+    self.tauXY = __viscosity * (dvdx + dvdy)
+    self.tauYX = __viscosity * (dvdx + dvdy)
+
 
 
   def computeVelocity(self, neigborList: list['Partical']):
-    dvdtX = 0;
-    dvdtY = 0;
+    dvxdt = 0;
+    dvydt = 0;
 
-    for other in neigborList:
-      velXDiff = self.velX - other.velX
-      velYDiff = self.velY - other.velY
+    for point in neigborList:
+      dwdx, dwdy = kenelFcunction.kenelDerivative(self, point)
+      temp = point.density / (self.density * point.density)
 
-      dwdx, dwdy = self.kenelFunction2derivative(other);
-      dpdtofNeigbor = self.density * other.mass / other.density \
-       * (velXDiff * dwdx + velYDiff * dwdy)
-      dpdt += dpdtofNeigbor
+      dvxdt += - temp * (self.pressure + point.pressure) * dwdx
+      dvxdt += temp * (self.tauXX + point.tauXX) * dwdx
+      dvxdt += temp * (self.tauXY + point.tauXY) * dwdy
 
-    return dvdtX, dvdtY
+      dvydt += - temp * (self.pressure + point.pressure) * dwdy
+      dvydt += temp * (self.tauYY + point.tauYY) * dwdy
+      dvydt += temp * (self.tauYX + point.tauYX) * dwdx
 
+    return dvxdt, dvydt
+  
+
+
+  #the followings are deprecated
+  def computePressureGradient(self, neigborList: list['Partical']) -> float:
+    #the important is to confine the direction of the pressure
+    #this use the pressure gradient equation from the papar of MPS
+    #and plus the power of mass 
+    gradientX = 0
+    gradientY = 0
+
+    for point in neigborList:
+      w = self.kenelFunction(point)
+      distance = math.sqrt( (self.posX - point.posX) **2 + (self.posY - point.posY) **2 )
+      temp = point.mass / point.density * w * (self.pressure - point.pressure) / distance
+      gradientX += temp * (self.posX - point.posX)
+      gradientY += temp * (self.posY - point.posY)
+
+    return gradientX, gradientY
+
+
+
+  def computeViscousForceGradient(self, neigborList: list['Partical']):
+    gradientX = 0
+    gradientY = 0
+
+    for point in neigborList:
+      d2wdx2, d2wdy2, d2wdxdy = kenelFcunction.kenelDerivative2Order(self, point)
+
+      #inclue the normal stress and the shear stress
+      vfX = __viscosity * point.mass / point.density \
+        * (point.velX * d2wdxdy + point.velY * d2wdy2 \
+        + 4/3 * point.velX * d2wdx2 - 2/3 * point.velY * d2wdxdy)
+      vfY = __viscosity * point.mass / point.density \
+        * (point.velY * d2wdxdy + point.velX * d2wdx2 \
+        + 4/3 * point.velY * d2wdy2 - 2/3 * point.velX * d2wdxdy)
+      gradientX += vfX
+      gradientY += vfY
+
+    return gradientX, gradientY
+
+
+
+  def computeTotalForceGradient(self, neigborList: list['Partical']):
+    pressureGradientX, pressureGradientY = self.computePressureGradient(neigborList)
+    viscousForceGradientX, viscousForceGradientY = self.computeViscousForceGradient(neigborList)
+
+    self.beForceGradientX = -pressureGradientX + viscousForceGradientX
+    self.beForceGradientY = -pressureGradientY + viscousForceGradientY
--- SphDemo/kenelFcunction.py
+++ SphDemo/kenelFcunction.py
@@ -2,15 +2,15 @@
 import Partical
 
 
-__h: float = 10
-__ad = 15 / ( 7 * math.pi * __h **2)
+__h: float = 10.0
+__ad:float = 15 / ( 7 * math.pi * __h **2)
 #in original method the r = |xi - xj| / h, ∇∇r will become very complacated
-#here I assume r = (xi - xj)^2 / h2, to simplify the computation
+#to simplify the computation, how about to assume r = (xi - xj)^2 / h^2 ? 
 
 
 
 def kenel(self: Partical.Partical, other: Partical.Partical) -> float:
-  r = ((self.posX - other.posX) **2 + (self.posY - other.posY) **2) / __h **2
+  r = math.sqrt((self.posX - other.posX) **2 + (self.posY - other.posY) **2) / __h
   
   w = 0;
   if r < 1 :
@@ -23,28 +23,43 @@ def kenel(self: Partical.Partical, other: Partical.Partical) -> float:
 
 
 def kenelDerivative(self: Partical.Partical, other: Partical.Partical):
-  r = ((self.posX - other.posX) **2 + (self.posY - other.posY) **2) / __h **2
-  dwdr = __ad * (3/2 * r **2 - 2 * r)   #partial derivative of w by r
+  rh = math.sqrt((self.posX - other.posX) **2 + (self.posY - other.posY) **2)
+  r = rh / __h
 
-  drdx = -2 * (self.posX - other.posX) / __h **2  #partial derivative of r by x
-  drdy = -2 * (self.posY - other.posY) / __h **2  #partial derivative of r by y
+  dwdx, dwdy = 0
+  if r >= 2:
+    pass
 
-  dwdx = dwdr * drdx
-  dwdy = dwdr * drdy
-  return dwdx, dwdy
-
-
-
-def kenelDerivative2order(self: Partical.Partical, other: Partical.Partical):
-  r = ((self.posX - other.posX) **2 + (self.posY - other.posY) **2) / __h **2
-  dwdr = __ad * (3/2 * r **2 - 2 * r)   #partial derivative of w by r
-  d2wdr2 = __ad * (3 * r - 2)   #partial derivative of w by r
-
-  divide = math.sqrt(other.posX **2 - 2 * self.posX * other.posX + self.posX **2 \
-    + other.posY **2 - 2 * self.posY * other.posY + self.posY **2) * __h
-  drdx = - (self.posX - other.posX) / divide  #partial derivative of r by x
-  drdy = - (self.posY - other.posY) / divide  #partial derivative of r by y
+  drdx = 2 * (self.posX - other.posX) / (__h * rh)  #partial derivative of r by x
+  drdy = 2 * (self.posY - other.posY) / (__h * rh)  #partial derivative of r by y
+  if r < 1:
+    dwdr = __ad * (3/2 * r **2 - 2 * r)   #partial derivative of w by r
+  if r < 2:
+    dwdr = - __ad * 1/2 * (2 - r) **2
 
   dwdx = dwdr * drdx
   dwdy = dwdr * drdy
   return dwdx, dwdy
+
+
+
+#deprecated
+def kenelDerivative2Order(self: Partical.Partical, other: Partical.Partical):
+  rh = math.sqrt((self.posX - other.posX) **2 + (self.posY - other.posY) **2)
+  r = rh / __h
+  dwdr = __ad * (3/2 * r **2 - 2 * r)   #partial derivative of w by r
+  d2wdr2 = __ad * (3 * r - 2)
+
+  drdx = 2 * (self.posX - other.posX) / (rh * __h)  #partial derivative of r by x
+  drdy = 2 * (self.posY - other.posY) / (rh * __h)  #partial derivative of r by y
+
+  d2rdx2 = (2 / rh - 4 * (self.posX - other.posX) **2 / rh **3) / __h
+  d2rdy2 = (2 / rh - 4 * (self.posY - other.posY) **2 / rh **3) / __h
+  d2rdxdy = 4 * (self.posX - other.posX) * (self.posY - other.posY) / rh **3 / __h
+
+  #d2wdx2 / dx2 = d2w / dr2 * dr / dx * dr / dx + dw / dx * d2r / dx2
+  d2wdx2 = d2wdr2 * drdx * drdx + dwdr * d2rdx2
+  d2wdy2 = d2wdr2 * drdy * drdy + dwdr * d2rdy2
+  d2wdxdy = d2wdr2 * drdx * drdy + dwdr * d2rdxdy
+
+  return d2wdx2, d2wdy2, d2wdxdy
